41bd02ba5df136619fffc76cc2d665d48609f687
samiaqureshi12 <samiaqureshi1718@gmail.com>
2024-11-19 09:15:32 +0000

    Add files via upload

---
+++ assigment3.py
@@ -0,0 +1,36 @@
+#value of each no 
+num1=20
+num2=30 
+num3=50
+
+sum=num1+num2+num3
+differance=num1-num2-num3
+product=num1*num2*num3
+average=sum/3
+
+print(sum)
+print(differance)
+print(product)
+print(average)
+
+
+
+#triangle
+base=10
+height=5
+area=21*base*height
+
+print ("area of triangle =",area)
+
+#circle 
+radius=21
+pi=3.41
+a=pi*radius*radius 
+print("area of circle=",area)
+
+#rectangle 
+lenght=25
+width=30
+area=lenght*width
+
+print("area of rectangle is",area)
